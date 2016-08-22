3f81aefd6b0624d6e79a8d46b20fa2903c29a7bd
Emanoel Lopes <emanoelopes@gmail.com>
2016-08-22 10:13:53 -0700

    Primeiro commit

---
+++ apostila-caelum/restaurantes.rb
@@ -0,0 +1,43 @@
+class Franquia
+        def initialize 
+                @restaurantes = []
+        end
+
+        def adiciona(*restaurante) #permite adicionar vários parametros
+                for restaurante in @restaurantes
+                        @restaurantes << restaurante
+                end
+        end 
+
+        def mostra
+                for restaurante in @restaurantes
+                        puts restaurante.nome
+                end
+        end
+
+end
+
+class Restaurante
+                attr_accessor :nome
+        
+        def fechar_conta(dados)
+                puts "Conta fechada no valor de #{dados[:valor]} e com nota #{dados[:nota]}. Comentário: #{dados[:comentario]}"
+        end
+
+end
+
+
+        restaurante_um = Restaurante.new
+        restaurante_um.nome = "Fasano"
+
+        restaurante_dois = Restaurante.new
+        restaurante_dois.nome = "Fogo de Chao"
+
+        franquia = Franquia.new
+
+        franquia.adiciona restaurante_um, restaurante_dois 
+
+        franquia.mostra
+
+        restaurante_um.fechar_conta :valor => 50, :nota => 9, :comentario => "Gostei!"
+
