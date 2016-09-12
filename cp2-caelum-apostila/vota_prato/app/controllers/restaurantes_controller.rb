62b8f67a54ea64d003485fad917b4d5074fc6263
Emanoel Lopes <emanoelopes@gmail.com>
2016-09-12 14:29:28 -0300

    Exercicio da pg 132

--- cp2-caelum-apostila/vota_prato/app/controllers/restaurantes_controller.rb
+++ cp2-caelum-apostila/vota_prato/app/controllers/restaurantes_controller.rb
@@ -14,6 +14,17 @@ class RestaurantesController < ApplicationController
 	def new
 		@restaurante = Restaurante.new
 	end
+	
+	#action create é necessária para persistir os dados no banco.
+	def create
+		@restaurante = Restaurante.new restaurante_params
+		@restaurante.save
+		redirect_to(action: "show", id: @restaurante)
+	end
 
+	#Solicita a atribuição das propriedades de um restaurante.
+	def restaurante_params 
+		params.require(:restaurante).permit(:nome, :endereco, :especialidade)
+	end
 
 end
