103567e6f3d368f7e9533f5fdd1b545725131ec5
Emanoel Lopes <emanoelopes@gmail.com>
2016-10-17 14:15:44 -0300

    Preparando para deploy

---
+++ app/listeners/qualifica_listener.rb
@@ -0,0 +1,5 @@
+class ClassificaListener
+	def qualificacao_creation_successful
+		puts "Um restaurante foi qualificado."		
+	end
+end
---
+++ app/views/franquias/_franquia.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! franquia, :id, :created_at, :updated_at
+json.url franquia_url(franquia, format: :json)
