c81453bacbe3cd20ef2fdc752562c13529a53586
Emanoel Lopes <emanoelopes@gmail.com>
2017-01-16 16:58:49 -0300

    pequena melhoria no menu principal

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ VotaPrato::Application.routes.draw do
   resources :comentarios
   resources :qualificacoes
   resources :clientes
+  resources :users
   match 'ola' => 'ola_mundo#index', via: 'get'
   match 'hello_world' => 'hello_world#index', via: 'get'
   get 'rack', :to => proc {|env| [200, {"Content-Type" => "text/html"}, ["App Rack numa rota Rails"]]}
